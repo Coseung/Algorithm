46dd2d621a9b7ffb0c5e23433e0a7e464fa10541
Coseung <149292617+Coseung@users.noreply.github.com>
2026-03-24 20:46:19 +0900

    [Bronze I] Title: 평균, Time: 276 ms, Memory: 22876 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int N = sc.nextInt();
+        double[] scores = new double[N];
+        double max = 0;
+        double sum = 0;
+        for (int i = 0; i < N; i++) {
+            scores[i] = sc.nextDouble();
+            if (scores[i] > max) {
+                max = scores[i];
+            }
+        }
+        for (int j = 0; j < N; j++) {
+            sum += (scores[j] / max) * 100;
+        }
+        System.out.println(sum / N);
+    }
+}
